0763ae670ee6abb00765857977e55ee7edbf49a5
nemezide <psy12@yandex.ru>
2025-11-29 13:34:57 +0500

    first view template

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,14 +1,16 @@
 package org.example.hexlet;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
         var app = Javalin.create(config -> {
+            config.fileRenderer(new JavalinJte());
             config.bundledPlugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+
+        app.get("/", ctx -> ctx.render("index.jte"));
+        app.start(7070);
     }
 }
